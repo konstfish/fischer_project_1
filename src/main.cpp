ac3868cae3b7679195aa93d5d3cd8e60282d78a6
David Fischer <david@konst.fish>
2020-11-29 21:00:32 +0100

    Add CLI input handling complete with range check

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,23 @@
 #include <iostream>
 #include <thread>
 
+#include "CLI11.hpp"
+
 using namespace std;
 
-//int main(int argc, char* argv[]) {
-int main() {
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    CLI::App app("Simulation einer verteilten Synchronisation mit einem zentralen Koordinator");
+
+    int no_of_nodes{3};
+
+    app.add_option("number", no_of_nodes, "Number of Nodes to create") -> required() 
+    ->  check(CLI::Range(2,100).description("range is limited to sensible values").active(true).name("range"));
+
+    // app.add_flag("-a, --socket", io_async, "Use sockets to communicate");
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) { 
+        return app.exit(e);
+    }
 }
