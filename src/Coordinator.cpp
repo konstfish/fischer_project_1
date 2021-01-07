e2e79653f6e68857692482d8c0b791372edf4da1
David Fischer <david@konst.fish>
2021-01-07 10:36:47 +0100

    More minor changes to fix errors when compiling on Ubuntu

--- src/Coordinator.cpp
+++ src/Coordinator.cpp
@@ -172,7 +172,7 @@ void Coordinator::output_stat_table(){
     int tmp = duration.count();
 
     if(tmp > 3){
-        cout << Final Stats: << endl;
+        cout << "Final Stats:" << endl;
         std::cout << generate_stat_table() << endl;
     }
 }
